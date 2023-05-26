71efbf975ee7d856912e8a908e57bbe5c4baecfd
ifybliss98 <133233202+ifybliss98@users.noreply.github.com>
2023-05-26 20:26:39 +0100

    Add files via upload

---
+++ PHP_my_data.php
@@ -0,0 +1,11 @@
+<?php
+$name = "<h1>KALU IFEOMA</h1>";
+$regno = "2021/HND/33377/CS<br>";
+$soo = "ABIA STATE<br>";
+$project = "<strong>WEB BASED PROJECT SUPERVISION SYSTEM WITH
+EMAIL NOTIFICATION</strong>";
+echo $name;
+echo $regno;
+echo $soo;
+echo $project;
+?>
